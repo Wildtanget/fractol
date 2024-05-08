16cffcd4484138dd90d5ed88f79c21407541cf27
Noureddine Tahtahi <noureddinetahtahi@hotmail.com>
2024-05-08 23:17:21 +0200

    Fixed colormp

--- fractol.c
+++ fractol.c
@@ -1,8 +1,10 @@
 #include "fractol.h"
 
-int main(void)
+int main(int argc, char **argv)
 {
     t_fractal fractal;
+
+    check_arguments(argc, argv, &fractal);
     init_fractal(&fractal);
     render(&fractal);
     mlx_hook(fractal.win_ptr, DestroyNotify, 0, (void *)close_program, &fractal);
@@ -12,35 +14,55 @@ int main(void)
     return (0);
 }
 
+void check_arguments(int argc, char **argv, t_fractal *fractal)
+{
+    if (argc == 1)
+    {
+        printf("Please specify a set.\nUsage: ./a.out \"set\"\n");
+        exit(1);
+    }
+    else if (ft_strncmp(argv[1], "mandelbrot", 10) == 0)
+        fractal->set = MANDELBROT;
+    else if (ft_strncmp(argv[1], "julia", 5) == 0)
+        fractal->set = JULIA;
+    else if (ft_strncmp(argv[1], "burning_ship", 12) == 0)
+        fractal->set = BURNING_SHIP;
+}
+
+int ft_strncmp(char *s1, char *s2, int length)
+{
+    if (s1 == NULL || NULL == s2 || length <= 0)
+        return (1);
+    while (*s1 == *s2 && length > 0)
+    {
+        s1++;
+        s2++;
+        length--;
+    }
+    return (*s1 - *s2);
+}
+
 void    init_fractal(t_fractal *fractal)
 {
     fractal->mlx_ptr = mlx_init();
-    if (!(fractal->mlx_ptr))
-    {
-        printf("Malloc error!");
-        exit(1);
-    }
-    fractal->win_ptr = mlx_new_window(fractal->mlx_ptr, WIDTH, HEIGHT, "test");
-    if (!(fractal->win_ptr))
-    {
-        printf("Malloc error!");
-        exit(1);
-    }
+    if (fractal->mlx_ptr == NULL)
+        handle_error(1);
+    fractal->win_ptr = mlx_new_window(fractal->mlx_ptr, WIDTH, HEIGHT, "mandelbrot");
+    if (fractal->win_ptr == NULL)
+        handle_error(2);
     fractal->img.img = mlx_new_image(fractal->mlx_ptr, WIDTH, HEIGHT);
-    if (!(fractal->img.img))
-    {
-        printf("Malloc error!");
-        exit(1);
-    }
+    if (fractal->img.img == NULL)
+        handle_error(3);
     fractal->img.addr = mlx_get_data_addr(fractal->img.img, &fractal->img.bpp, &fractal->img.len, &fractal->img.endian);
-    if (!(fractal->img.addr))
-    {
-        printf("Malloc error!");
-        exit(1);
-    }
+    if (fractal->img.addr == NULL)
+        handle_error(4);
     fractal->offset_x = 0;
     fractal->offset_y = 0;
     fractal->scale = 1;
+    fractal->iterations = 40;
+    fractal->color.r = 255;
+    fractal->color.g = 0;
+    fractal->color.b = 0;
 }
 
 void render(t_fractal *fractal)
@@ -48,7 +70,7 @@ void render(t_fractal *fractal)
     double x;
     double y;
     t_complex temp;
-    int iterations;
+    int escape_num;
     x = 0;
     while (x < WIDTH)
     {
@@ -57,9 +79,10 @@ void render(t_fractal *fractal)
         {
             temp.x = (map(x, -2, +2, 0, WIDTH) * fractal->scale) + fractal->offset_x;
             temp.y = (map(y, +2, -2, 0, HEIGHT) * fractal->scale) - fractal->offset_y;
-            iterations = iterater(temp);
-            if (iterations < 40)
-                place_pixel(fractal, x, y, 0x000000 + 0xFFFFFF * ((double)iterations / 200));
+            if (fractal->set == MANDELBROT)
+                escape_num = iterater(temp, fractal);
+            if (escape_num < fractal->iterations)
+                place_pixel(fractal, x, y, scale_color(escape_num, fractal));
             else
                 place_pixel(fractal, x, y, 0x000000);
             y++;
@@ -95,22 +118,21 @@ t_complex square_complex(t_complex z)
     return (result);
 }
 
-int iterater(t_complex c)
+int iterater(t_complex c, t_fractal *fractal)
 {
-    int iterations = 40;
     int i = 0;
     t_complex z;
     z.x = 0.0;
     z.y = 0.0;
 
-    while (i < iterations)
+    while (i < fractal->iterations)
     {
         z = sum_complex(square_complex(z), c);
         if ((z.x * z.x) + (z.y * z.y) > 4)
             return (i);
         i++;
     }
-    return (iterations);
+    return (fractal->iterations);
 }
 
 void close_program(t_fractal *fractal)
@@ -129,7 +151,7 @@ void close_program(t_fractal *fractal)
 
 int keyhook(int keycode, t_fractal *fractal)
 {
-    printf("%d was pressed\n", keycode);
+    // printf("%d was pressed\n", keycode);
     if (keycode == XK_Escape)
         close_program(fractal);
     else if (keycode == XK_Left || keycode == XK_a)
@@ -140,14 +162,20 @@ int keyhook(int keycode, t_fractal *fractal)
         fractal->offset_y -= (.5 * fractal->scale);
     else if (keycode == XK_Down || keycode == XK_s)
         fractal->offset_y += (.5 * fractal->scale);
+    else if (keycode == XK_minus)
+        fractal->iterations -= 10;
+    else if (keycode == XK_equal)
+        fractal->iterations += 10;
+    else if (keycode >= XK_1 && keycode <= XK_9)
+        shift_color(keycode, fractal);
     render(fractal);
     return (0);
 }
 
 int mousehook(int button, int x, int y, t_fractal *fractal)
 {
-    printf("%d was pressed at (%d, %d)\n", button, x, y);
-    if (button == 4)
+    // printf("%d was pressed at (%d, %d)\n", button, x, y);
+    if (button == 4 && x && y)
         fractal->scale *= 0.9;
     if (button == 5)
         fractal->scale *= 1.1;
@@ -158,4 +186,54 @@ int mousehook(int button, int x, int y, t_fractal *fractal)
 double map(double unscaled_num, double new_min, double new_max, double old_min, double old_max)
 {
     return (new_max - new_min) * (unscaled_num - old_min) / (old_max - old_min) + new_min;
+}
+
+void handle_error(int code)
+{
+    printf("Failed with code: %d", code);
+    exit(1);
+}
+
+int scale_color(int escape_num, t_fractal *fractal) {
+    int hex_color;
+
+    double t = (double)escape_num / fractal->iterations;
+    int r = (int)(fractal->color.r * t);
+    int g = (int)(fractal->color.g * t);
+    int b = (int)(fractal->color.b * t);
+
+    hex_color = r << 16 | g << 8 | b;
+    return hex_color;
+}
+
+void shift_color(int keycode, t_fractal *fractal)
+{
+    if (keycode == XK_1)
+        fractal->color = hex_to_rgb(NEON_GREEN);
+    else if (keycode == XK_2)
+        fractal->color = hex_to_rgb(ELECTRIC_BLUE);
+    else if (keycode == XK_3)
+        fractal->color = hex_to_rgb(HOT_PINK);
+    else if (keycode == XK_4)
+        fractal->color = hex_to_rgb(DEEP_PURPLE);
+    else if (keycode == XK_5)
+        fractal->color = hex_to_rgb(ACID_YELLOW);
+    else if (keycode == XK_6)
+        fractal->color = hex_to_rgb(FIERY_ORANGE);
+    else if (keycode == XK_7)
+        fractal->color = hex_to_rgb(TURQUOISE);
+    else if (keycode == XK_8)
+        fractal->color = hex_to_rgb(CRIMSON_RED);
+    else if (keycode == XK_9)
+        fractal->color = hex_to_rgb(ROYAL_BLUE);
+}
+
+t_rgb hex_to_rgb(int hex)
+{
+    t_rgb color;
+
+    color.r = (hex >> 16) & 0xFF;
+    color.g = (hex >> 8) & 0xFF;
+    color.b = hex & 0xFF;
+    return (color);
 }
--- fractol.h
+++ fractol.h
@@ -10,6 +10,26 @@
 # define WIDTH 640
 # define HEIGHT 640
 
+# define MANDELBROT 1
+# define JULIA 2
+# define BURNING_SHIP 3
+
+#define NEON_GREEN 0x39FF14
+#define ELECTRIC_BLUE 0x00BFFF
+#define HOT_PINK 0xFF69B4
+#define DEEP_PURPLE 0x9400D3
+#define ACID_YELLOW 0xC2FF00
+#define FIERY_ORANGE 0xFF8C00
+#define TURQUOISE 0x40E0D0
+#define CRIMSON_RED 0xDC143C
+#define ROYAL_BLUE 0x4169E1
+
+typedef struct s_rgb {
+    int r;
+    int g;
+    int b;
+}   t_rgb;
+
 typedef struct s_image {
     void    *img;
     char    *addr;
@@ -22,9 +42,12 @@ typedef struct s_fractal {
     void    *mlx_ptr;
     void    *win_ptr;
     t_image img;
+    int     set;
     double  offset_x;
     double  offset_y;
     double  scale;
+    int     iterations;
+    t_rgb   color;
 }   t_fractal;
 
 typedef struct s_complex {
@@ -32,14 +55,20 @@ typedef struct s_complex {
     double y;
 }   t_complex;
 
-void    init_fractal(t_fractal *fractal);
-void    render(t_fractal *fractal);
-void    place_pixel(t_fractal *fractal, int x, int y, int color);
-int iterater(t_complex c);
-t_complex square_complex(t_complex z);
-t_complex sum_complex(t_complex z1, t_complex z2);
-void close_program(t_fractal *fractal);
-int keyhook(int keycode, t_fractal *fractal);
-int mousehook(int button, int x, int y, t_fractal *fractal);
-double map(double unscaled_num, double new_min, double new_max, double old_min, double old_max);
+void        init_fractal(t_fractal *fractal);
+void        render(t_fractal *fractal);
+void        place_pixel(t_fractal *fractal, int x, int y, int color);
+int         iterater(t_complex c, t_fractal *fractal);
+t_complex   square_complex(t_complex z);
+t_complex   sum_complex(t_complex z1, t_complex z2);
+void        close_program(t_fractal *fractal);
+int         keyhook(int keycode, t_fractal *fractal);
+int         mousehook(int button, int x, int y, t_fractal *fractal);
+double      map(double unscaled_num, double new_min, double new_max, double old_min, double old_max);
+void check_arguments(int argc, char **argv, t_fractal *fractal);
+int         ft_strncmp(char *s1, char *s2, int length);
+void handle_error(int code);
+int scale_color(int escape_num, t_fractal *fractal);
+t_rgb hex_to_rgb(int color);
+void shift_color(int keycode, t_fractal *fractal);
 #endif
